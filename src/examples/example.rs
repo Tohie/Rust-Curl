19ab991481e9473a841a2a52f2dd77bb11337c44
Scott Jenkins <scottdjwales@gmail.com>
2014-04-23 11:54:46 +0100

    Removed doc dir and added it to gitignore

--- src/examples/example.rs
+++
@@ -1,8 +0,0 @@
-extern crate curl;
-
-fn main() {
-    let req = curl::Request::new();
-    println!("{}", req.get("http://www.google.co.uk"));
-    println!("{}", req.get("http://www.facebook.co.uk"));
-    req.download("http://www.google.co.uk", "google.html");
-}
